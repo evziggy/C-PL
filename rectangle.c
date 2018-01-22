1ce4f7202eb9524d4092cb60f660d9487febb40c
Evense Zetrenne <ezetrenne@gmail.com>
2018-01-22 14:07:36 -0500

    Add rectangle via upload

---
+++ rectangle.c
@@ -0,0 +1,44 @@
+/**************************************************************************/
+/* Program that calculates the area and perimeter of a rectangle. 
+/* Coded by: Evense Zetrenne
+/* For: Program Design
+/**************************************************************************/
+#include <stdio.h>
+
+void area_perimeter(double width, double length, double *area, 
+double *perimeter);
+
+int main(void){
+
+double width, length, are, peri;
+
+    printf("Enter the width of the rectangle: ");
+if (scanf("%lf",&width) != 1) {
+fputs("read error width\n", stderr);
+return 1;
+
+}
+
+    printf("Enter the length of the rectangle: ");
+if (scanf("%lf",&length) != 1) {
+fputs("read error length\n", stderr);
+return 1;
+
+}
+
+ area_perimeter(width, length, &are, &peri);
+
+    printf("The area of the rectangle is: %f\n",are);
+    printf("The perimeter of the rectangle is: %f\n",peri);
+
+return 0;
+
+} 
+
+void area_perimeter(double width, double length, double *area, double *perimeter){
+
+*area = length * width;
+
+*perimeter = (length + width)*2;
+
+}
